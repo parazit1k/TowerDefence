a2b855835b8868e669d45ce0a3f71cc8e36629d6
parazitik <parazit3333@gmail.com>
2023-11-26 15:27:30 +0300

    some changes x2

--- scripts/playing_area.gd
+++ scripts/playing_area.gd
@@ -4,7 +4,11 @@ extends Node2D
 @export var enemy = preload("res://screens/enemy.tscn")
 
 @onready var spawner = $Path2D/PathFollow2D
+@onready var pathSpawner = $Path2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawner.add_child(enemy.instantiate())
+	for i in range(10):
+		pathSpawner.add_child(spawner)
+		spawner.add_child(enemy.instantiate())
+		get_tree().create_timer(1).timeout
